9be1b7964eed2250e29dad7feb4568474334c4eb
Roxana Angheluta <anghelutar@github.com>
2019-09-17 08:38:23 +0200

    DOCKER-268: add check_validity script as one of the entrypoint scripts to run. Looks at environment variable BUILD_DATE and warns on stdout if it is not set or if the date there is more than 30 days in the past.

---
+++ src/main/docker/docker-entrypoint.d/89-check-validity.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+currentDate=`date +%s`
+if [ -z "${BUILD_DATE}" ]
+then
+    1>&2 echo "************"
+    1>&2 echo "This is an old image: BUILD_DATE is not set"
+    1>&2 echo "*************"
+else
+    timeelapsed=`expr ${currentDate} - ${BUILD_DATE}`
+    if [ $timeelapsed -ge 2592000 ] # 30 days
+    then
+	1>&2 echo "************"
+	1>&2 echo "This is an old image: BUILD_DATE=$(date -d @$BUILD_DATE +"%Y-%m-%d %H:%M:%S") and currentDate=$(date -d @$currentDate +"%Y-%m-%d %H:%M:%S")"
+	1>&2 echo "*************"
+    fi
+fi
+
+
