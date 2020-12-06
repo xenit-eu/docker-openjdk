e70c9b478a73fc36a9356244517e3261216ceb82
koen <koen.vangeerteruy@gmail.com>
2020-12-06 02:02:43 +0100

    DOCKER-357: Fix message 'tput: No value for xterm-256color and no -T specified'

--- src/main/docker/docker-entrypoint.d/89-check-validity.sh
+++ src/main/docker/docker-entrypoint.d/89-check-validity.sh
@@ -3,17 +3,15 @@
 currentDate=`date +%s`
 if [ -z "${BUILD_DATE}" ]
 then
-    1>&2 echo "************"
-    1>&2 echo "This is an old image: BUILD_DATE is not set"
-    1>&2 echo "************"
+    1>&2 echo "$(tput -Txterm setaf 3)****************************************************$(tput -Txterm sgr0)"
+    1>&2 echo "$(tput -Txterm setaf 3)Unable to determine image age: BUILD_DATE is not set$(tput -Txterm sgr0)"
+    1>&2 echo "$(tput -Txterm setaf 3)****************************************************$(tput -Txterm sgr0)"
 else
     timeelapsed=`expr ${currentDate} - ${BUILD_DATE}`
     if [ $timeelapsed -ge 2592000 ] # 30 days
     then
-	1>&2 echo "************"
-	1>&2 echo "This is an old image: BUILD_DATE=$(date -d @$BUILD_DATE +"%Y-%m-%d %H:%M:%S") and currentDate=$(date -d @$currentDate +"%Y-%m-%d %H:%M:%S")"
-	1>&2 echo "************"
+	1>&2 echo "$(tput -Txterm setaf 3)****************************************************************************************$(tput -Txterm sgr0)"
+	1>&2 echo "$(tput -Txterm setaf 3)This is an old image: BUILD_DATE=$(date -d @$BUILD_DATE +"%Y-%m-%d %H:%M:%S") and currentDate=$(date -d @$currentDate +"%Y-%m-%d %H:%M:%S")$(tput -Txterm sgr0)"
+	1>&2 echo "$(tput -Txterm setaf 3)****************************************************************************************$(tput -Txterm sgr0)"
     fi
 fi
-
-
